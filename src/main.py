c8e208a5cd5b66aae1f4404306bc6b09a8709df4
Theo <theo.lisin@gmail.com>
2023-07-27 15:00:26 +0300

    load datasets functionality and clean functions

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import sly_globals as g
 from utils import pages_to_images_from_folder_or_file, download_project
 
 
-def convert_pdf_to_images_on_agent(api: sly.Api):
+def convert_pdf_to_images_on_agent(api: sly.Api) -> Path:
     local_project_path = download_project(api, g.INPUT_PATH)
     
     for folder_or_file in local_project_path.iterdir():
@@ -23,12 +23,13 @@ def convert_pdf_to_images_on_agent(api: sly.Api):
             g.DPI,
             api.logger
         )
-    
+    return local_project_path
+
 
 @sly.timeit
 def import_pdf_as_img(api: sly.Api, task_id: int):
     dir_info = api.file.list(g.TEAM_ID, g.INPUT_PATH)
-    convert_pdf_to_images_on_agent(api)
+    local_pdf_path = convert_pdf_to_images_on_agent(api)
 
     if len(dir_info) == 0:
         raise Exception(f"There are no files in selected directory: '{g.INPUT_PATH}'")
@@ -47,9 +48,48 @@ def import_pdf_as_img(api: sly.Api, task_id: int):
     else:
         project = api.project.get_info_by_id(g.PROJECT_ID)
 
-    if g.IS_ON_AGENT:
-        sly.logger.info(f"Data will be downloaded: {g.INPUT_PATH}")
-        download_project(api, g.INPUT_PATH)
+    for dataset_path in g.CONVERTED_LOCAL_DIR.iterdir():
+        dataset_name = dataset_path.name
+        dataset_info = api.dataset.create(
+            project_id=project.id,
+            name=dataset_name,
+            change_name_if_conflict=True,
+        )
+        img_paths = list(dataset_path.iterdir())
+        img_names = [p.name for p in img_paths]
+
+        batch_size = 10
+        total = len(img_names)
+        progress = sly.Progress(
+            f"Uploading images to dataset {dataset_name}",
+            total_cnt=total
+        )
+
+        for pos, (batch_names, batch_paths) in enumerate(zip(
+            sly.batched(seq=img_names, batch_size=batch_size),
+            sly.batched(seq=img_paths, batch_size=batch_size),
+        )):
+            batch_names = [str(bn) for bn in batch_names]
+            api.image.upload_paths(
+                dataset_id=dataset_info.id,
+                names=batch_names,
+                paths=batch_paths,
+            )
+            current = max(pos * batch_size, total)
+            progress.set(current, total, report=True)
+        
+        progress.iters_done_report()
+    
+    sly.fs.remove_dir(dir_=str(g.CONVERTED_LOCAL_DIR))
+    sly.fs.remove_dir(dir_=str(local_pdf_path))
+
+    if g.REMOVE_SOURCE and not g.IS_ON_AGENT:
+        api.file.remove(team_id=g.TEAM_ID, path=g.INPUT_PATH)
+        source_dir_name = Path(g.INPUT_PATH).parent.name
+        api.logger.info(msg=f"Source directory: '{source_dir_name}' was successfully removed.")
+    
+    api.task.set_output_project(task_id=task_id, project_id=project.id, project_name=project.name)
+
 
 
 if __name__ == "__main__":
