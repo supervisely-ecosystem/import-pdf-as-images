2ae33636c0cb711bc21c3ba96b5a6a8d3a62b07c
Theo <theo.lisin@gmail.com>
2023-07-27 12:47:28 +0300

    local storage path and debug env added

--- src/sly_globals.py
+++ src/sly_globals.py
@@ -1,11 +1,14 @@
 import os
+from pathlib import Path
 
 import supervisely as sly
 from dotenv import load_dotenv
 from supervisely.io.fs import mkdir
 
+
 if sly.is_development():
     load_dotenv("local.env")
+    load_dotenv("debug.env")
     load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 api: sly.Api = sly.Api.from_env()
@@ -17,14 +20,15 @@ WORKSPACE_ID = sly.env.workspace_id()
 PROJECT_ID = sly.env.project_id(raise_not_found=False)
 DATASET_ID = sly.env.dataset_id(raise_not_found=False)
 
-
 INPUT_PATH = os.environ.get("modal.state.files", None)
 if INPUT_PATH is None or INPUT_PATH == "":
     INPUT_PATH = os.environ.get("modal.state.slyFolder")
 
+
 OUTPUT_PROJECT_NAME = os.environ.get("modal.state.project_name", "")
-DPI = int(os.environ.get("model.state.dpi"), 300)
+DPI = int(os.environ.get("model.state.dpi", 300))
 
 DEFAULT_DATASET_NAME = "ds0"
 
 STORAGE_DIR = sly.app.get_data_dir()
+CONVERTED_LOCAL_DIR = Path(STORAGE_DIR) / "results"
